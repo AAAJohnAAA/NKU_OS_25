502a79a582fa95a7b2b8866499fbe63be0148440
zhangtianli <zhangtianli2023@outlook.com>
2025-10-29 15:36:56 +0800

    lab3练习一代码完成

--- lab3/kern/trap/trap.c
+++ lab3/kern/trap/trap.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <trap.h>
 
+#include <sbi.h>
+
 #define TICK_NUM 100
 
 static void print_ticks() {
@@ -124,12 +126,30 @@ void interrupt_handler(struct trapframe *tf) {
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
             // cprintf("Supervisor timer interrupt\n");
-             /* LAB3 EXERCISE1   YOUR CODE :  */
+             /* LAB3 EXERCISE1   YOUR CODE :  2310894
+             */
             /*(1)设置下次时钟中断- clock_set_next_event()
              *(2)计数器（ticks）加一
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
+            // 设置下次时钟中断 
+            clock_set_next_event();
+            // 计数器（ticks）加一
+            {
+                static int ticks = 0;
+                static int print_num = 0;
+                ticks++;
+                // 当计数器加到 TICK_NUM 的倍数的时候，打印并累加打印次数
+                if (ticks % TICK_NUM == 0) {
+                    print_ticks();
+                    print_num++;
+                }
+                // 当打印次数为 10 时，调用 sbi_shutdown 关机
+                if (print_num >= 10) {
+                    sbi_shutdown();
+                }
+            }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
@@ -163,19 +183,27 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB3 CHALLENGE3   YOUR CODE :  */
+             /* LAB3 CHALLENGE3   YOUR CODE :  2310894*/
             /*(1)输出指令异常类型（ Illegal instruction）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Illegal instruction\n");
+            cprintf("  epc      0x%08x\n", tf->epc);
+            // 跳过非法指令
+            tf->epc += 4;
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
-            /* LAB3 CHALLLENGE3   YOUR CODE :  */
+            /* LAB3 CHALLLENGE3   YOUR CODE :  2310894*/
             /*(1)输出指令异常类型（ breakpoint）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Breakpoint\n");
+            cprintf("  epc      0x%08x\n", tf->epc);
+            // 断点后
+            tf->epc += 4;
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
